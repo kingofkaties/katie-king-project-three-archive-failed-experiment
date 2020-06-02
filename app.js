fdadd72d9bd9a1f1915a8b990a1d3fc958480328
Katie King <kingofkaties@gmail.com>
2020-06-02 18:38:47 -0400

    Initial commit

---
+++ app.js
@@ -0,0 +1,9 @@
+// Be Still landing page with settings for customizing your meditation session
+
+// User clicks on indicated keywords/buttons to loop through preset transition options held inside an object, changing the contents of "length" variable as user cycles through
+
+// "begin" button applies class of "hidden" to settings and "active" to animation div, changing what information is displayed on the page and accessing preset properties within "active" class
+
+// Use JS to insert "length" variable as transition-duration CSS property on the animation div
+
+// "adjust" button brings settings back into focus so user can adjust settings, changing the "length" variable
