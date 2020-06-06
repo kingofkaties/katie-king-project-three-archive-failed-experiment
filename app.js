92b839874538afe45b79ee16155737522132326d
Katie King <kingofkaties@gmail.com>
2020-06-06 00:00:20 -0400

    Removed text from HTML and added to JS as variables/properties

--- app.js
+++ app.js
@@ -52,12 +52,35 @@ beStill.toggleTheme = [
     },
 ];
 
-let breathLength = 0;
-let colorSelect = 0;
-let themeSelect = 0;
+// set index variables
+beStill.breathLength = 0;
+beStill.colorSelect = 0;
+beStill.themeSelect = 0;
+
+// set text variables
+beStill.themeText = $('<div>').html('<h2 class="showHide active">among the <a href="" class="toggle theme">simplicity</a></h2>');
+beStill.colorText = $('div').html('<p>click the <a class="toggle color" href="">red</a> text to see your options</p>');
+beStill.breathText = $('<div>').html('<p>inhale for <a class="toggle inhale" href="">3</a> seconds</p>');
+beStill.soundText = $('<div>').html('<p>sound is <a class="toggle" href="">on</a></p>')
+beStill.begin = $('<div>').html('<h3><a class= "activate toggle" href=""> begin</a></h3 >');
+beStill.settings = $('<div>').html(beStill.themeText, bsStill.colorText, beStill.breathText, beStill.soundText,
+    beStill.beginText);
+beStill.meditate = $('<div>').html('<div class="animation showHide meditate dormant"></div>
+')
 
 $('.inhale').text(beStill.toggleBreath[0]);
 $('.toggle').css('color', beStill.toggleColor[0].colorHex);
+
+// TK create a function to automate checking the index and resetting to zero if needed, not working yet
+// beStill.resetArrayIndex = function(index, array) {
+//     if (index === array.length) {
+//         index = 0;
+//     } else {
+//         index++;
+//     };
+//     console.log(index);
+//     return index;
+// }
 
 beStill.init = function() {
     // hide and show settings/animation
@@ -79,35 +102,38 @@ beStill.init = function() {
     $('.inhale').on('click', function(e) {
         e.preventDefault();
         // cycle up through available breath lengths and reset to zero once end of array is reached
-        breathLength += 1;
-        if (breathLength === beStill.toggleBreath.length) { breathLength = 0 };
+        // TK calling function to check index number, but function not working yet
+        // beStill.resetArrayIndex(beStill.breathLength, beStill.toggleBreath);
+        // console.log(beStill.breathLength);
+        beStill.breathLength += 1;
+        if (beStill.breathLength === beStill.toggleBreath.length) { beStill.breathLength = 0 };
         // change text to reflect current breath length
-        $(this).text(beStill.toggleBreath[breathLength])
+        $(this).text(beStill.toggleBreath[beStill.breathLength])
         // change value of CSS animation property to reflect current breath length
         $('.animation')
-            .css('animation-duration', `${beStill.toggleBreath[breathLength]}s`)
+            .css('animation-duration', `${beStill.toggleBreath[beStill.breathLength]}s`)
     });
     
     // cycle through button colours on click
     $('.color').on('click', function(e){
         e.preventDefault();
         // cycle up through available colors, resetting to zero once end of array is reached
-        colorSelect += 1;
-        if (colorSelect === beStill.toggleColor.length) { colorSelect = 0 };
-        $(this).text(beStill.toggleColor[colorSelect].colorText);
-        $('.toggle').css('color', beStill.toggleColor[colorSelect].colorHex)
+        beStill.colorSelect += 1;
+        if (beStill.colorSelect === beStill.toggleColor.length) { beStill.colorSelect = 0 };
+        $(this).text(beStill.toggleColor[beStill.colorSelect].colorText);
+        $('.toggle').css('color', beStill.toggleColor[beStill.colorSelect].colorHex)
     })
     
     // cycle through themes on click
     $('.theme').on('click', function(e){
         e.preventDefault();
         // cycle through themes, resetting to zero once end of array is reached
-        themeSelect += 1;
-        if (themeSelect === beStill.toggleTheme.length) { themeSelect = 0 };
-        $(this).text(beStill.toggleTheme[themeSelect].name);
-        console.log(beStill.toggleTheme[themeSelect].name);
+        beStill.themeSelect += 1;
+        if (beStill.themeSelect === beStill.toggleTheme.length) { beStill.themeSelect = 0 };
+        $(this).text(beStill.toggleTheme[beStill.themeSelect].name);
+        console.log(beStill.toggleTheme[beStill.themeSelect].name);
         $('body')
-            .css('background-image', `url('images/${beStill.toggleTheme[themeSelect].image}')`)
+            .css('background-image', `url('images/${beStill.toggleTheme[beStill.themeSelect].image}')`)
             // .css('background-size', '100%')
             // .css('background-position', 'center');
     })
